16d83eb44548ed3bd6d211371e117d827b5af6f2
Joan Iglesias <joan@iglesias.com>
2021-03-25 19:56:16 +0100

    Added the utility macro expansion. Now tested with the current main_.go file. Also modified the Makefile to compile the macro_expansion.go

--- utilities/macro_expansion.go
+++ utilities/macro_expansion.go
@@ -8,6 +8,34 @@ import (
 	"strings"
 	"regexp"
 )
+// macro line
+type mLine struct {
+	line	string
+	subs	bool		// must be inserted here a line
+}
+func generateCode(out *os.File,macro []mLine,funcLines []string,varLines []string) {
+
+	for i,_:= range funcLines { // for each function to expand:
+		first := true
+		for _,line := range macro {
+
+			if first {	// begining of a function expansion
+				if line.subs {
+					fmt.Fprintln(out, funcLines[i])
+					first = false
+				} else {
+					fmt.Fprintln(out, line.line)
+				}
+			} else {	// var expansions
+				if line.subs {
+					fmt.Fprintln(out, varLines[i])
+				} else {
+					fmt.Fprintln(out, line.line)
+				}
+			}
+		}
+	}
+}
 
 func main () {
 
@@ -28,8 +56,6 @@ func main () {
 	out, err := os.Create(argsWithoutProg[0][:len(argsWithoutProg[0])-5] + ".go")
     if err != nil { panic(err) }
 
-
-
 	file	:= string(fileToParse)
 
 	lines	:= strings.Split(file,"\n")
@@ -41,44 +67,73 @@ func main () {
 	// detection of the end of the macro
 	macroEnd:= regexp.MustCompile(`^//<<end>>`)	// capturing var variables into the functions
 
+	macro		:= []mLine{}		// the whole function storing and each line to be substituted masked with a boolean
+	funcLines	:= []string{}		// func style lines
+	varLines	:= []string{}		// var lines
+	beginMacro	:= false
+
 	for i:=0;i<len(lines);i++ {
 
-		res := funcDec.FindStringSubmatch(line[i])
-		if len(res) > 0 {		// detection of the func macro
-			var macro		:= []string		// the whole function storing
-			var funcLines	:= []string		// func style lines
-			var varLines	:= []string		// var lines
-
-			macro = append(macro,line)		// storing the func macro detection
-			for (j:=i;i<len(lines);j++) {	// macro reading and storing
-
-
+		res := funcDet.FindStringSubmatch(lines[i])
+		if len(res) == 2  {		// detection of the func macro
+			beginMacro = true
+			// while there are macro head lines we must store them:
+			macro		= append(macro,mLine{lines[i],false})		// storing the func macro detection
+			funcLines	= append(funcLines,res[1])
+			fmt.Fprintln(out, lines[i])
+			i++
+			for j:=i;j<len(lines);j++ {	// macro reading and storing
+				res = funcDet.FindStringSubmatch(lines[j])
+				if len(res)!= 2 {		// no more matches -> break the macro 
+					macro		= append(macro,mLine{lines[j],true})		// storing the "hole" for the function expansion
+					fmt.Fprintln(out, lines[j])
+					i=j+1
+					break
+				} else {
+					macro		= append(macro,mLine{lines[j],false})		// storing the func macro detection
+					funcLines	= append(funcLines,res[1])
+					fmt.Fprintln(out, lines[j])
+				}
 			}
+			// end storing macro head lines
 
-		}
-		fmt.Fprintln(out, line)
+		}// we exit this block with the first "actual" func line (not a macro one) stored in the macro slice
 
-		res := re1.FindStringSubmatch(line)
-		if len(res) ==  2 {
-			fmt.Fprintln(out, res[1] + "if r.Error != nil { goto ErrorTrack }")
-			continue
-		}
+		res = varDet.FindStringSubmatch(lines[i])
+		if len(res) == 2 {
+			// while there are macro variables lines we must store them:
+			macro		= append(macro,mLine{lines[i],false})
+			varLines	= append(varLines,res[1])
+			fmt.Fprintln(out, lines[i])
+			i++
+			for j:=i;j<len(lines);j++ {	// macro reading and storing
+				res = varDet.FindStringSubmatch(lines[j])
+				if len(res) != 2 {		// no more matches -> break the macro 
+					macro		= append(macro,mLine{lines[j],true})
+					fmt.Fprintln(out, lines[j])
+					i=j+1
+					break
+				} else {
+					macro		= append(macro,mLine{lines[j],false})
+					varLines	= append(varLines,res[1])
+					fmt.Fprintln(out, lines[j])
+				}
+			}
+			// end storing macro var lines
 
-		res = re2.FindStringSubmatch(line)
-		if len(res) == 3 {
-			fmt.Fprintln(out, res[1] +"if "+ res[2] +".Error != nil { goto ErrorTrack_"+ res[2] +" }")
-			continue
-		}
-		res = re3.FindStringSubmatch(line)
-		if len(res) ==  2 {
-			fmt.Fprintln(out, res[1] + "if err != nil { goto ErrorTrack_err }")
-			continue
-		}
+		}// we exit this block with the first "actual" var line (not a macro one) stored in the macro slice
 
-		res = re4.FindStringSubmatch(line)
-		if len(res) == 3 {
-			fmt.Fprintln(out, res[1] +"if "+ res[2] +" != nil { goto ErrorTrack_"+ res[2] +" }")
-			continue
+		res = macroEnd.FindStringSubmatch(lines[i])
+		if len(res) == 1 {
+			beginMacro	= false
+			fmt.Fprintln(out, lines[i])
+			generateCode(out,macro,funcLines,varLines)
+		}
+		if beginMacro	{			// inside the macro processing => no output lines
+			macro		= append(macro,mLine{lines[i],false})		// storing actual code
+			fmt.Fprintln(out, lines[i])
+		}else			{			// no inside a macro processing => writting to the general output.
+			fmt.Fprintln(out, lines[i])
 		}
 	}
 
